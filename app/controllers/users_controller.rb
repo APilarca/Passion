1e4f93dae62aaa2b6e8f15d11bba03d357584cf4
Angelica Pilarca <email": null>
2016-09-26 19:18:11 -0700

    create controller for users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+get '' do
+  erb :'/users/new'
+end
+
+post '/useres/new' do
+  @user =User.new(params)
+  if @user.save
+    login(@user)
+
+    redirect "/usres/#{@user.id}"
+  else
+    @error = "Sorry, please try again!"
+
+    erb :'/users/new'
+  end
+end
+
+get '/users/:id' do
+  if session[:user_id]
+    @user = User.find(session[:user_id])
+  end
+
+  erb :'/users/show'
+end
