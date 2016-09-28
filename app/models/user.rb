234b28cade034db713fbdf1c1f9f40060cf784c6
Angelica Pilarca <email": null>
2016-09-27 17:34:03 -0700

    fix models, and make sure login and logout are working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,25 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  has_many :drink_sessions
+  # users.password_hash in the database is a :string
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(email, password)
+    @user = User.find_by(email: email)
+    if @user && @user.password == password
+      @user
+    else
+      nil
+    end
+  end
 end
